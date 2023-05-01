1d328369e08ee566b0d4b71a8dd1600092a252e6
Mr.Shadow <131967950+MrShadows01@users.noreply.github.com>
2023-05-02 00:01:51 +0330

    Update CLPM.py

--- CLPM.py
+++ CLPM.py
@@ -2,20 +2,39 @@
 from os import system
 import sys
 from colorama import Fore
+import time 
+def GetTime():
+    x = time.localtime()
+    year = x.tm_year
+    Month = x.tm_mon
+    Day = x.tm_mday
+    Hour = x.tm_hour
+    Mints = x.tm_min
+    Secound = x.tm_sec
+    Time = "{}/{}/{} - {}:{}:{}".format(year, Month,Day,Hour,Mints,Secound)
+    return Time
 # ● Symbol
 # Class Methods For CLPM(Command line Project Mannager)
 class CLPM(object):
     def __init__(self) :
         pass
     def Create_Projects(self,Name,Description,Time): # For Create New Project
-        pass
+        print("Create New Project This Information \nProject Name {}\nDescription {}\nTime {}\n".format(Name,Description,Time))
+        ReadFileLog = open("LogFile.txt","r")
+        LOgFiles = open("LogFile.txt","+a")
+        Pro_name = ReadFileLog.read()
+        if(Name in Pro_name):
+            print("Project Exists")
+            LOgFiles.write("\nCLPM Closed in {}\t (because Project Existed) ".format(GetTime()))
+            
+        else:
+            LOgFiles.write("\nProject {} Created in {} \nDescription :{}".format(Name,Time,Description))
+            LOgFiles.close()
     def Delete_Projects(self,Name): # For Delete Project
         pass
     def Show_Projects():# Show Projects List
         pass 
     def Edit_Projects(self,Name): # For Edit Project
-        pass
-    def Show_log():
         pass
 # Class Methods For Run CLPM on Command Line
 class RUN(object):
@@ -33,10 +52,16 @@ class RUN(object):
             pass  
 # Class Methods For Error Messages
 class Error(object):
-    Err0 = "Command Not Found Please try -help"
+    def Err0(text):
+        Err0 = Fore.RED+"Error 0x01 Commnad Not Found ({}) ".format(text)+Fore.RESET
+        return Err0
+def Show_log():
+    ShowLog = open("LogFile.txt","r")
+    Log = ShowLog.read()
+    print(Fore.GREEN+f"{Log}"+Fore.RESET)
 # Def For Help Messages
 def Help():
-    system("cls"or"clear")
+    
     print("Usage: CLPM <Command> [options]")
     print("\tExample: CLPM -L (show Your Project List)")
     print("\tCommand\t\tDescription")
@@ -50,24 +75,35 @@ def Help():
     print("\t"+"{"+Fore.GREEN+"@"+Fore.RESET+"}"+" -help     \tFor Show help")
 # Start CLPM 
 if __name__ == "__main__":
-    CLPM = CLPM()
+    LogFile = open("LogFile.txt","+a")
+    Time = GetTime()
+    LogFile.write("\nOpen CLPM {}".format(Time))
+    LogFile.close()
+    CLPMs = CLPM()
     Argv = sys.argv
+    Argv1 = Argv[1]
     if(len(Argv) <= 1):
         RUN = RUN()
     else:
-        if(Argv[1] == "-help"):
+        if(Argv1.lower() == "-help"):
+            system("cls"or"clear")
             Help()
-        if(Argv[1] == "-log"):
-            pass
-        if(Argv[1] == "-e"):
-            pass
-        if(Argv[1] == "-l"):
-            pass
-        if(Argv[1] == "-d"):
-            pass
-        if(Argv[1] == "-Add"):
-            pass
+        elif(Argv1.lower() == "-log"):
+            Show_log()
+        elif(Argv1.lower() == "-e"):
+            print("-e")
+        elif(Argv1 == "-l"):
+            print("-l")
+        elif(Argv1.lower() == "-d"):
+            print("-d")
+        elif(Argv1.lower() == "-add"):
+            ProjectName = Argv[2]
+            PrjectDescription = Argv[3].replace("/"," ")
+            Time = GetTime()
+            CLPMs.Create_Projects(ProjectName, PrjectDescription,Time)           
         else:
+            system("cls"or"clear")
+            print(Error.Err0(Argv1))
             Help()
             
         
